08d60ea0dcf27e9156d566788fe7200fa7e3647b
Ubuntu <ubuntu@ip-172-31-32-143.us-east-2.compute.internal>
2019-08-15 19:25:09 +0000

    asignar gps a vehiculo master

--- storage/framework/views/6bff5071085dfd6f9ba42a43eb36c86262d38b54.php
+++ storage/framework/views/6bff5071085dfd6f9ba42a43eb36c86262d38b54.php
@@ -4,20 +4,13 @@
         <button data-pushbar-close><i class="fas fa-times"></i></button>
     </div>
     <nav class="menu">
+        <a href="/cat_cliente">Modulo Clientes</a>
+        <a href="/cat_vehiculos">Modulo Vehículos</a>
         <a href="/">Inicio (Mapa)</a>
-        <a href="/cat_tipopersonas">ABC Tipo Persona</a>
-        <a href="/cat_tipoempresas">ABC Tipo Empresa</a>
-        <a href="/cat_tiposervicios">ABC Tipo Servicio</a>
-        <a href="/cat_estadoclientes">ABC Estados Cliente</a>
-        <a href="/cat_tipodomicilios">ABC Domicilios</a>
-        <a href="/cat_clientes">ABC Clientes</a>
-        <a href="/cat_tipoempleados">ABC Tipo Empleados</a>
-        <a href="/cat_estadosusuario">ABC Estados Usuarios</a>
-        <a href="/cat_usuarios">ABC Usuarios</a>
-        <a href="/cat_estadosturnos">ABC Estados Turnos</a>
-        <a href="/cat_tipoturnos">ABC Tipo Turnos</a>
-        <a href="/cat_turnos">ABC Turnos</a>
-        <a href="/cat_estadosrespveh">ABC Estados Responsables Vehículos</a>
-        <a href="/cat_respveh">ABC Responsables de Vehículos</a>
+        <a href="/cat_cliente">Modulo Clientes</a>
+        <a href="/cat_vehiculo">Modulo Vehículos</a>
+        <a href="/cat_usuario">Modulo Usuarios</a>
+        <a href="/cat_responsables">Modulo Responsables</a>
+        <a href="/cat_gps">Modulo GPS</a>
     </nav>
 </div><?php /**PATH /var/www/html/monitoreo/resources/views/template/menu_catalogos.blade.php ENDPATH**/ ?>
